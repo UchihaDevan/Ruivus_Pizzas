a23c53a5c97757092131ad4860ef002f1c0588e4
error: 404 <45114712+UchihaDevan@users.noreply.github.com>
2024-11-19 20:42:16 -0300

    Limpeza de arquivos

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-mobile').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+});
